843a7ade37f6953ddb3b48a479f317a087e2cb22
Graham Keeth <graham.keeth@gmail.com>
2023-07-09 15:04:15 -0400

    BCD instead of hex

--- stopwatch/stopwatch.v
+++ stopwatch/stopwatch.v
@@ -47,7 +47,10 @@ module top (
 
 	end
 
-	assign display_value_inc = display_value + 8'b1;
+        bcd8_increment bcd_inc (
+                .din(display_value),
+                .dout(display_value_inc)
+        );
 
 	// 7 segment display control Pmod 1A
 	seven_seg_ctrl seven_segment_ctrl (
